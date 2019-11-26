63940cc840d76cde85c39a25c58f65c716332b9d
howif <714171792@qq.com>
2019-11-26 16:43:16 +0800

    part done

--- time_assist_routine.c
+++ time_assist_routine.c
@@ -19,3 +19,43 @@ int main(){
     printf("microsecond:%ld\n",tv.tv_sec*1000000 + tv.tv_usec);  //微秒
     return 0;
 }
+
+/* mark all the timestamps in conn; used when conn is init */
+void markConnStamp(conn_obj *connInfo)
+{
+    task_obj *taskObj;
+    iop_list_node_t *taskNode;
+    unsigned long start_time;
+#ifdef __linux__
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    start_time = tv.tv_sec * 1000 + tv.tv_usec / 1000; /* time in millisecond*/
+#else
+
+#endif
+    if (conInfo == NULL)
+    {
+        printf("Error: Invalid conection\n");
+	return;
+    }
+
+    taskNode = connInfo->task_list.head;
+    while(taskNode)
+    {
+        taskObj = (task_obj)*taskNode->data;
+        if (taskObj == NULL)
+	{
+	    printf("Error: Invalid task object in node\n");
+	    break;
+	}	
+#ifdef __linux__
+        taskObj->start_time =         
+
+#else
+#endif
+    }
+
+
+
+
+}
